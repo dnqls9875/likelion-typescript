7fbbb247225cc3e14d7f22d41da85db663212316
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2025-01-07 10:46:18 +0900

    [실습] - getter, setter, protected

--- src/chapter/09-classes/07-getter-setter.ts
+++ src/chapter/09-classes/07-getter-setter.ts
@@ -6,39 +6,83 @@
 // ------------------------------------------------------------------------------
 
 // score 프로퍼티를 읽을 수 있도록 설정합니다.
-// 단, 직접 score 프로퍼티 값을 수정할 수는 없습니다.
+// 단, 직접 score 프로퍼티 값을 수정할 수는 없습니다. Getter
 
 // fullName 프로퍼티를 구성합니다.
 // - getter를 사용해 `role nickname` 조합 값을 반환하도록 합니다.
 // - setter를 사용해 role 또는 nickname 값을 수정할 수 있도록 구현합니다.
 
 // FullName 타입 별칭이 필요한 경우 추가합니다.
+type FullName = string | { role: string; nickname: string };
 
 {
   class Player {
     constructor(
-      private readonly nickname: string,
-      private readonly role: string,
-      private score: number = 0
+      private nickname: string,
+      private role: string,
+      private _score: number = 0
     ) {
       this.boostScoreUp();
     }
 
+    get fullName() {
+      // return this.role + " " + this.nickname;
+      // return `${this.role} ${this.nickname}`;
+      const { role, nickname } = this;
+      return `${role} ${nickname}`;
+    }
+
+    set fullName(option: FullName) {
+      // options이 string인 경우 :
+      if (typeof option === "string") {
+        const [role, nickname] = option.split(" "); // ['role', 'nickname']
+        this.role = role;
+        this.nickname = nickname;
+      }
+      // options이 object인 경우 :
+      else {
+        const { role, nickname } = option;
+        this.updateFullName(role, nickname);
+      }
+    }
+
+    private updateFullName(role: string, nickname: string): void {
+      this.role = role;
+      this.nickname = nickname;
+    }
+
+    // READONLY
+    get score() {
+      return this._score;
+    }
+
+    // set score(newScore: number) {
+    //   this._score = newScore;
+    // }
+
     private boostScoreUp(): void {
-      this.score += 100;
+      this._score += 100;
     }
 
     public scoreUp(point: number): void {
-      this.score += point;
+      this._score += point;
     }
 
     public scoreDown(point: number): void {
-      this.score -= point;
+      this._score -= point;
     }
   }
 
-  const yamoo9 = new Player('yamoo9', '멘토');
+  const yamoo9 = new Player("yamoo9", "멘토");
 
+  // READ
   console.log(yamoo9.score);
+
+  // WRITE
+  // yamoo9.score = 100;
+
+  console.log(yamoo9.fullName);
+
+  yamoo9.fullName = "instructor yamoo9";
   console.log(yamoo9.fullName);
 }

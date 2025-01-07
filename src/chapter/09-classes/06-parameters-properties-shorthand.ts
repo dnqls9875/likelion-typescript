9a79fbee10bf7d6c0e7050ec411a1b011c2eaa95
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2025-01-07 10:21:33 +0900

    [실습] - 파라미터 프로퍼티 숏핸드

--- src/chapter/09-classes/06-parameters-properties-shorthand.ts
+++ src/chapter/09-classes/06-parameters-properties-shorthand.ts
@@ -9,11 +9,14 @@
 
 {
   class Player {
-    public readonly nickname: string;
-    public readonly role: string;
+    // public readonly nickname: string;
+    // public readonly role: string;
     private score: number = 0;
 
-    constructor(nickname: string, role: string) {
+    constructor(
+      public readonly nickname: string,
+      public readonly role: string
+    ) {
       this.nickname = nickname;
       this.role = role;
       this.boostScoreUp();
@@ -32,8 +35,8 @@
     }
   }
 
-  const yamoo9 = new Player('yamoo9', '멘토');
+  const yamoo9 = new Player("yamoo9", "멘토");
 
-  yamoo9.boostScoreUp();
-  console.log(yamoo9.score);
+  // yamoo9.boostScoreUp();
+  // console.log(yamoo9.score);
 }

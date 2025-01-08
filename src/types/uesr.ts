0d808c2a16f85ba3854c460874066e3c41272369
이우빈 <125638564+dnqls9875@users.noreply.github.com>
2025-01-08 14:24:11 +0900

    [실습] - 타입스크립트 유틸리티 메서드
    - Pick, Omit

--- src/types/uesr.ts
+++ src/types/uesr.ts
@@ -1,9 +1,13 @@
-interface User {
+export interface User {
   id: number;
   name: string;
   gender: string;
-  education: string;
   age: number;
 }
+
+// ? 특정한 속성을 빼오는 방법은? 타입스크립트 유틸리티 Pick를 사용해라
+// * 특정한 속성을 제외하는 방법은? 타입스크립트 유틸리티 Omit을 사용해라
+// export type RequestUser = Pick<User, "age" | "gender" | "name">;
+export type RequestUser = Omit<User, "id">;
 
 export default User;
